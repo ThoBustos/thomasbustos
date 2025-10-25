8b2132977c84a2949785af1671b68ac684b4f333
Thomas <thomasbustos@lets-talk-ai.com>
2025-10-26 00:02:04 +0200

    updated pattern

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,7 @@ function App() {
     <div className="container">
       {/* Background layer - animation */}
       <div className="background-layer">
-        <PixelBlast
-          variant="rectangular"
-          pixelSize={6}
-          color="#B19EEF"
-          patternScale={3}
-          patternDensity={1.2}
-          pixelSizeJitter={0.5}
-          enableRipples={false}
-          rippleSpeed={0.4}
-          rippleThickness={0.12}
-          rippleIntensityScale={1.5}
-          liquid
-          liquidStrength={0.12}
-          liquidRadius={1.2}
-          liquidWobbleSpeed={5}
-          speed={0.6}
-          edgeFade={0.25}
-          transparent
-        />
+        <PixelBlast />
       </div>
       
       {/* Foreground layer - content */}
